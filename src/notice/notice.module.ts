3e1f65ed547e5efd37989632a888dfa57076d4d5
ariffaysal <ariffaysal001@gmai.com>
2026-02-25 12:29:36 +0600

    aasa

--- src/notice/notice.module.ts
+++ src/notice/notice.module.ts
@@ -1,22 +1,3 @@
-// import { Module, forwardRef } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { NoticeService } from './notice.service';
-// import { NoticeController } from './notice.controller';
-// import { Notice } from './entities/notice.entity';
-// import { WhatsappService } from '../whatsapp.service'; 
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forFeature([Notice]),
-//   ],
-//   controllers: [NoticeController],
-//   providers: [NoticeService, WhatsappService],
-//   exports: [NoticeService]
-// })
-// export class NoticeModule {}
-
-
-
 
 
 import { Module } from '@nestjs/common';
@@ -24,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { NoticeService } from './notice.service';
 import { NoticeController } from './notice.controller';
 import { Notice } from './entities/notice.entity';
-import { Setting } from './entities/setting.entity'; // Make sure this import is here
+import { Setting } from './entities/setting.entity'; 
 import { WhatsappService } from '../whatsapp.service'; 
 
 @Module({
